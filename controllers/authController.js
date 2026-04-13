ecd0160cd0918d26c76cc743920c37121cab8bcc
unknown <mohamedishaaqmeeran@gmail.com>
2026-04-13 21:55:14 +0530

    completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,26 +26,22 @@ const saveFCMToken = async (req, res) => {
 const authController = {
     register: async (req, res) => {
         try {
-            // get the details from the request body
+        
             const { name, email, password } = req.body;
-
-            // check if the user exists already
+          
             const existingUser = await User.findOne({ email });
 
             if (existingUser) {
                 return res.status(400).json({ message: 'User already exists' });
             }
 
-            // encrypt the password
+           
             const hashedPassword = await bcrypt.hash(password, 10);
 
-            // create a new user
             const newUser = new User({ name, email, password: hashedPassword });
 
-            // save the user to the database
             await newUser.save();
 
-            // send an email to the user
              await sendEmail(email, 'Welcome to our app', 'Thank you for registering!');
 
             res.status(201).json({ message: 'User registered successfully' });
@@ -57,28 +53,27 @@ const authController = {
         try {
             const { email, password } = req.body;
 
-            // find the user by email
             const user = await User.findOne({ email });
 
             if (!user) {
                 return res.status(400).json({ message: 'Email does not exist' });
             }
 
-            // compare the password
+          
             const isMatch = await bcrypt.compare(password, user.password);
 
             if (!isMatch) {
                 return res.status(400).json({ message: 'Invalid password' });
             }
 
-            // generate a JWT token
+        
             const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '3h' });
 
-            // set the token as a cookie
+            
             res.cookie('token', token, {
                httpOnly: true,
-    secure: true,        // MUST be true on Render HTTPS
-    sameSite: 'none',  // 24 hours
+    secure: true,        
+    sameSite: 'none',  
             })
 
             return res.status(200).json({
@@ -97,19 +92,18 @@ const authController = {
     },
     getMe: async (req, res) => {
         try {
-            // get the user ID
+            
             const userId = req.userId;
 
-            // find the user by ID
-            // const user = await User.findById(userId).select('-password').populate('assignedCompany', 'name');
+         
             const user = await User.findById(userId).select('-password');
 
-            // if the user does not exist, return an error
+            
             if (!user) {
                 return res.status(404).json({ message: 'User not found' });
             }
 
-            // return the user details
+       
             res.status(200).json({ user });
         } catch (error) {
             res.status(500).json({ message: 'Error fetching user details', error: error.message });
