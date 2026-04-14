9fa281a1742e726bd6b5946405258f4893f5ab4e
unknown <mohamedishaaqmeeran@gmail.com>
2026-04-14 13:25:55 +0530

    final

--- utils/email.js
+++ utils/email.js
@@ -1,20 +1,26 @@
-const { Resend } = require('resend');
-
-const resend = new Resend(process.env.GOOGLE_APP_PASSWORD);
+const nodemailer = require('nodemailer');
+const { EMAIL_USER, GOOGLE_APP_PASSWORD } = require('../utils/config');
+console.log(EMAIL_USER)
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: EMAIL_USER,
+        pass: GOOGLE_APP_PASSWORD
+    }
+});
 
 const sendEmail = async (to, subject, text) => {
-    try {
-        const data = await resend.emails.send({
-            from: 'ishaaqmeeran1@gmail.com',
-            to,
-            subject,
-            text
-        });
-
-        console.log("✅ Email sent:", data);
-    } catch (error) {
-        console.log("❌ Email error:", error);
+    const mailOptions = {
+        from: EMAIL_USER,
+        to,
+        subject,
+        text,
     }
-};
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+    
+}
 
 module.exports = sendEmail;
